cfb92a108317924c66ec81b42a3d533202ee801f
SengHong SOKHON <sokhonsenghong8@gmail.com>
2026-05-28 08:49:24 +0700

    update Image Hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  basePath: "/v0-elt-education",
+  assetPrefix: "/v0-elt-education",
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
 }
-
-export default nextConfig
